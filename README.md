698379f32bb1fe23ac4a59c1eee104f09b484b7f
Aaron Moran <aaronmorann@protonmail.com>
2021-01-06 12:57:31 +0000

    UI updates, second model being saved and loaded in but 500 internal server error bug, README updates

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 # Emerging Technology Project - G00356519
 
+# Overview
+The goal of this project is to produce a model that accurately predicts wind turbine power output from wind speed
+values, as in the data set. A web service created using Flask responds with
+predicted power values based on speed values sent as HTTP requests.
+
 # Windows
 ```
 set FLASK_APP=server.py
 python -m flask run
-```
-
-```
-docker build . -t rando-image
-docker run --name rando-container -d -p 5000:5000 rando-image
 ```
 
 # Linux
@@ -19,5 +19,8 @@ python3 -m flask run
 
 ### References
 * [Ian McLoughlin's Github Repository - Keras Linear](https://github.com/ianmcloughlin/jupyter-teaching-notebooks/blob/master/keras-linear.ipynb)
+* [The Art of Routing in Flask](https://hackersandslackers.com/flask-routes/)
 * [Tensorflow Basic Regression](https://www.tensorflow.org/tutorials/keras/regression)
 * [Tensorflow Save and Load Models](https://www.tensorflow.org/tutorials/keras/save_and_load)
+* [Pickle loading anmd saving models](https://machinelearningmastery.com/save-load-machine-learning-models-python-scikit-learn/#:~:text=Saving%20Your%20Model-,Save%20Your%20Model%20with%20pickle,it%20to%20make%20new%20predictions.)
+* [Flask - POST Method](https://stackoverflow.com/questions/34853033/flask-post-the-method-is-not-allowed-for-the-requested-url)
--- server.py
+++ server.py
@@ -10,22 +10,41 @@ import pickle
 # Create a new web app.
 app = fl.Flask(__name__)
 
-@app.route('/', methods=["GET","POST"])
-def predict():
+@app.route('/')
+def root():
+    return render_template("index.html")
+
+
+@app.route('/keras', methods=["GET","POST"])
+def keras():
   if request.method == "POST":
     # Process input from user and cast to a float
-    speed = request.form["speed"]
-    speed=[float(speed)]
+    speed = [float(request.form["speed"])]
     
-    # Loading in the two models to compare accuracy
+    # Loading in the model to test accuracy
     model = tf.keras.models.load_model('models/model')
+    
+    # Predict using model 1
+    model_prediction = model.predict(speed)
+
+    return render_template("keras.html", output=model_prediction[0])
+  return render_template("keras.html")
+
+@app.route('/tree', methods=["GET","POST"])
+def tree():
+  if request.method == "POST":
+    # Process input from user and cast to a float
+    speed2 = [float(request.form["speed"])]
+    
+    # Loading in the model to test accuracy
     model2 = pickle.load(open('models/model2.sav', 'rb'))
     
-    # Predict using model 1 and model 2 - only model 1 working
-    model_prediction = model.predict(speed)
-    # model_prediction2 = model2.predict(speed)
-    return render_template("index.html", output=model_prediction[0])
-  return render_template("index.html")
+    # Predict using model 2
+    model_prediction2 = model2.predict(speed2)
+
+    return render_template("tree.html", output2=model_prediction2[0])
+  return render_template("tree.html")
 
 if __name__ == 'main':
+  app.debug = True
   app.run(port=5000, debug=True)
